c5d3b5896f2b800a90837880cb07a2257188b639
Joseph Martinez <jtm5317@gmail.com>
2019-01-26 19:11:20 -0600

    Removed Action Class

---
+++ src/kingdom/actions/InnerCity.java
@@ -0,0 +1,50 @@
+package kingdom.actions;
+
+import kingdom.Variables;
+
+public class InnerCity {
+    
+    public String getEmploymentStat() {
+        return Integer.toString(Variables.employmentStat);
+    }
+    
+    public String getPovertyStat() {
+        return Integer.toString(Variables.povertyStat);
+    }
+    
+    public String getCrimeStat() {
+        return Integer.toString(Variables.povertyStat);
+    }
+
+    public void GiveToPoor(double amt) {
+        
+        Variables.K_royalGold -= amt;
+
+        //SUPER MAD POOR Take the money and spit on you - HIGH chance to steal more
+        if (Variables.lowHappiness <= 15) {
+
+            if ((int) (Math.random() * 2) + 1 == 1) { //IF THEY STEAL MORE
+                double stolenGold = (Math.random() * 3);
+                Variables.K_royalGold -= stolenGold;
+                amt+= stolenGold;
+            }
+
+        } else if (Variables.lowHappiness <= 30) { //MAD POOR Take the money and spit on you  - FAIR chance to steal more
+
+            if ((int) (Math.random() * 6) + 1 == 1) { //IF THEY STEAL MORE
+                double stolenGold = (Math.random());
+                Variables.K_royalGold -= stolenGold;
+                amt+=stolenGold;
+            }
+
+        }
+        
+        Variables.lowTotalWealth += amt;
+
+    }
+
+ 
+    public void TalkToGuard() {
+        
+    }
+}
